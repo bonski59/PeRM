d6cbb2e00ac08534f9936bfa7842d4e1943508e4
bonski59 <60044219+bonski59@users.noreply.github.com>
2021-02-12 14:45:46 -0700

    Added email module, we can use this as a baseline for mass emailing what ever excel docs that need to be sent.

--- email_functions.py
+++ email_functions.py
@@ -31,17 +31,18 @@ def send_email(email_recipient,
                         "attachment; filename= %s" % filename)
         msg.attach(part)
 
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login('charliekil0159@gmail.com', 'ck1590!@')
+        text = msg.as_string()
+        server.sendmail(email_sender, email_recipient, text)
+        server.quit()
+    except:
+        print("SMPT server connection error")
+    return True
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('charliekil0159@gmail.com', 'ck1590!@')
-    text = msg.as_string()
-    server.sendmail(email_sender, email_recipient, text)
-    server.quit()
-
-
-    return
 
 
 send_email('blake.ufp@gmail.com',
@@ -51,47 +52,7 @@ send_email('blake.ufp@gmail.com',
 
 
 
+# reccomend sending confirmation email, i.e. adding your work email into the "TO:" line so you can see it on your phone if needed.
 
-
-
-
-"""import smtplib
-import email.utils
-from email.mime.text import MIMEText
-
-# Create the message
-msg = MIMEText('This is the body of the message.')
-msg['To'] = email.utils.formataddr(('Recipient', 'corbinkelly15@gmail.com'))
-msg['From'] = email.utils.formataddr(('Author', 'corbinkelly15@gmail.com'))
-msg['Subject'] = 'Simple test message'
-
-server = smtplib.SMTP('127.0.0.1', 1025)
-server.set_debuglevel(True) # show communication with the server
-try:
-    server.sendmail('corbinkelly15@gmail.com', ['corbinkelly15@gmail.com'], msg.as_string())
-finally:
-    server.quit()
-"""
-
-"""import os
-import smtplib  # Import smtplib for the actual sending function
-from email.message import EmailMessage  # Import the email modules we'll need
-
-textfile = r"{}\testemail.txt".format(os.getcwd())
-
-# Open the plain text file whose name is in textfile for reading.
-with open(textfile) as fp:
-    # Create a text/plain message
-    msg = EmailMessage()
-    msg.set_content(fp.read())
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = f'The contents of {textfile}'
-msg['From'] = "corbinkelly15@gmail.com"
-msg['To'] = "blake.ufp@gmail.com"
-
-# Send the message via our own SMTP server.
-s = smtplib.SMTP('localhost')
-s.send_message(msg)
-s.quit()"""
+# TODO: make email messaging time based
+# TODO: make email messaging execution based (send email to burner account, upon rx burner calls PeRM and emails explicit or implicit target
