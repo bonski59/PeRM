0f1b975b6ee69ab02f83e6268f534b804c4e0911
bonski59 <60044219+bonski59@users.noreply.github.com>
2021-02-12 11:55:08 -0700

    Adding refresh_excel module, this will allow us to execute updated information that is pulled by various excel documents.
    however this one is catered to

---
+++ refresh_excel.py
@@ -0,0 +1,23 @@
+#most of this will be a test environment to verify this work
+import os
+
+import win32com.client
+
+excelDoc = r"{}\mainExcel.xlsx".format(os.getcwd())
+
+def refresh():                                  # this is difficult to test due to my lack of experience in excel. however it should work as intended with the UFP files.
+    # Start an instance of Excel
+    xlapp = win32com.client.DispatchEx("Excel.Application")
+
+    # Open the workbook in said instance of Excel
+    wb = xlapp.workbooks.open(excelDoc)
+
+    # Optional, e.g. if you want to debug
+    # xlapp.Visible = True
+
+    # Refresh all data connections.
+    wb.RefreshAll()
+    wb.Save()
+
+    # Quit
+    xlapp.Quit()
