da9a77e6f2a8c2bedb01da7ad0a7b214782d2f64
bonski59 <60044219+bonski59@users.noreply.github.com>
2021-02-18 16:38:09 -0700

    WIP: Designing means of correcting filepaths in REPORT_DETAIL csv

--- folders.py
+++ folders.py
@@ -1,7 +1,8 @@
 # this shouldn't have any functions in it
 # only classes
-
 import os
+
+import pandas as pd
 
 """
 This a new technique I've been using. basically any important filepaths that we need to use 
@@ -25,4 +26,27 @@ class Paths:
     reportCSV = r"{}\REPORT_DETAIL.csv".format(cwd)  # use this for local ops
 
 
+def change_csv_xl_filepaths_to_local_directory():   # explicit function that interacts with LiveData\REPORT_DETAIL.csv
+    csv_path = r"{}\dataManagement\LiveData\REPORT_DETAIL.csv".format(os.getcwd())
+    function_cwd = r"{}\dataManagement\LiveData".format(os.getcwd())
+    live_reports = r"{}\dataManagement\LiveData\reports_xlsx".format(os.getcwd())
+    df = pd.read_csv(csv_path)
+    loop_length = int(df.shape[0]) - 1
+    # print(df['Report_Filepath'])
+    
+    def this_file_is_in_line(string):
+        local_files = os.listdir(live_reports)
+        print(local_files)
+        for i in local_files:
+            if i in string:
+                return i
+            else:
+                return False
 
+    for fp in df['Report_Filepath']:
+        print(fp)
+        val = this_file_is_in_line(str(fp))
+        if val:
+            return val
+
+change_csv_xl_filepaths_to_local_directory()
