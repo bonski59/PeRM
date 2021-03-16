a9e78e134a3400d65527692a13867cdf79e2b1eb
bonski59 <60044219+bonski59@users.noreply.github.com>
2021-03-16 07:01:31 -0600

    small additions to VENDORDRILL_DATA_CONNECTION.py

--- VENDORDRILL_DATA_CONNECTION.py
+++ VENDORDRILL_DATA_CONNECTION.py
@@ -2,7 +2,11 @@ import pandas as pd
 import pyodbc
 from azure_api import azureDevTools as adt
 from admin import *
+from configparser import ConfigParser
 
+config_file = r"{}\config.ini".format(os.getcwd())
+config = ConfigParser()
+config.read(config_file)
 
 def vendorDrillConnection(query_arr):  # designed to ingest array like "[[*.csv, *.txt], [*.csv, *.txt]]"
     for i in query_arr:
@@ -28,6 +32,17 @@ def vendorDrillConnection(query_arr):  # designed to ingest array like "[[*.csv,
         """
     return
 
+
+
+
+def vdc():
+    for i in config.sections():
+        if "query" in i:
+            dbc = config[i]['dbc']
+            q_arr = adt.siphon_blob_list(config[i]['query_txt'])
+            for query in q_arr:
+                cloud_query_dbc_to_csv(query=query, dbc=dbc)
+
     # query_lookup = adt.siphon_blob_list(query)
 
 
@@ -38,17 +53,10 @@ def cloud_query_dbc_to_csv(query, dbc):
     result_set = pd.read_sql(adt.blob_to_txt(query), pyodbc.connect("DSN={}".format(dbc), autoCommit=True))
     admin_print("Txt query sent to dbc: {}".format(query))
 
-    """ 
-    I did not write this part, it was received from Home Depot analysts 
-    From what I can tell the above variable sets a pandas data frame to "result_set" from reading a SQL 
-    query by using the text file from "query_arr[i][1]" and then python makes a database 
-    connection using the input DSN, and pulls the query info
-    -CK
-    """
     if not os.path.exists(folders.Paths.upload_path):
         os.mkdir(folders.Paths.upload_path)
 
-    result_set.to_csv(path_or_buf=leaf, index=False)
+    result_set.to_csv(path_or_buf=r"{}/{}".format(folders.Paths.upload_path, leaf), index=False)
     admin_print("CSV location created : {}".format(output_blob))
     return
 
